a3ddce547c5736eab679e0186ba126549d337028
kwon852456 <kwon852456@naver.com>
2018-12-10 08:41:14 +0000

    before new branch

--- opencv_proj/05_server_serial.cpp
+++ opencv_proj/05_server_serial.cpp
@@ -96,7 +96,7 @@ int main(int argc, char **argv)
 		while((msg_size = read(acc_sock , buffer , 1024) > 0)){
 			// cout << "Msg exists.." << endl;
 
-			write(fd,buffer,10);
+			// write(fd,buffer,10);
 			write(fd,"1",10);
 			cout << buffer <<endl;
 			// write(acc_sock , buffer , 1024);
